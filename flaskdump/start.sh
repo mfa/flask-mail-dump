da3c1fd8111ce68d7e4bb96d84131eeda1d49ad7
Andreas Madsack <andreas@madflex.de>
2015-06-21 18:14:15 +0200

    one is enough

--- flaskdump/start.sh
+++ flaskdump/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PYTHONUNBUFFERED=0
 mkdir -p /opt/code/logs
-exec uwsgi --http 0.0.0.0:5000 --wsgi-file main.py --master --processes 4 --threads 2
+exec uwsgi --http 0.0.0.0:5000 --wsgi-file main.py --master --processes 1 --threads 2
